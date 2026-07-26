4c46fe3470b8d098be5e9bf17583bf9e76ac9bae
Jeffrey Melloy <jmelloy@gmail.com>
2026-07-26 13:00:53 -0700

    chore: save uncommitted work before push [auto-commit] (#577)

--- backend/codex/main.py
+++ backend/codex/main.py
@@ -250,14 +250,14 @@ app.include_router(
     tags=["collaborators"],
 )
 app.include_router(
-    users.bots_router,
-    prefix="/api/v1/workspaces/{workspace_identifier}/bots",
-    tags=["bots"],
-)
-app.include_router(
     sync.router,
     prefix="/api/v1/workspaces/{workspace_identifier}/sync",
     tags=["sync"],
+)
+app.include_router(
+    users.bots_router,
+    prefix="/api/v1/workspaces/{workspace_identifier}/bots",
+    tags=["bots"],
 )
 app.include_router(integrations.router, prefix="/api/v1/plugins/integrations", tags=["integrations"])
 app.include_router(plugins.router, prefix="/api/v1/plugins", tags=["plugins"])
